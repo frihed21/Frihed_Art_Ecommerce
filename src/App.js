a03adf3d1d52175f6737bb2f353d0abe89a6dcfa
frihed21 <krafort2121@gmail.com>
2022-06-06 23:07:14 -0500

    Desafio CLI

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FRIHED ART ECOMMERCE</h1>
+      <h2>Tienda de Arte by Katerin Morales</h2>
+    </div>
+  );
+}
+
+export default App;
