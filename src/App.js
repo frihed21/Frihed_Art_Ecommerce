7877fcc9807a6fc149f812fb2c9684b25c61cc3b
frihed21 <krafort2121@gmail.com>
2022-06-09 22:00:44 -0500

    Menu navegacion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React, {Component} from 'react';
+import './style.css';
 import './App.css';
+import Navigation from './components/Navigation';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>FRIHED ART ECOMMERCE</h1>
-      <h2>Tienda de Arte by Katerin Morales</h2>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Navigation/>
+        <h1>FRIHED ART ECOMMERCE</h1>
+        <h2>Tienda de Arte by Katerin Morales</h2>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,38 @@
+import "./Navigation.css";
+
+const Navigation = () => {
+    return (     
+        
+        <nav class="header__container2">           
+            <ul class="header__list">
+                    <li class="header__item"><a class="header__link" href="./nosotros/nosotros.html">BIO</a></li>
+                    <li class="header__item header__item--hover"><a class="header__link" href="#">PINTURAS</a> 
+                        <ul class="header__sublist"> 
+                            <li class="header__subitem"><a class="header__sublink" href="./pinturas/acuarelas.html">ACUARELA</a></li>
+                            <li class="header__subitem"><a class="header__sublink" href="./pinturas/acrilicos.html">ACRILICO</a></li>
+                            <li class="header__subitem"><a class="header__sublink" href="./pinturas/digital.html">DIGITAL</a></li>
+                        </ul>
+                    </li>
+                    <li class="header__item header__item--hover"><a class="header__link" href="#">PAPELERIA</a> 
+                        <ul class="header__sublist"> 
+                            <li class="header__subitem"><a class="header__sublink" href="./papeleria/cuadernos.html">CUADERNOS</a></li>
+                            <li class="header__subitem"><a class="header__sublink" href="./papeleria/greetingcard.html">GREETING CARD SET</a></li>
+                        </ul>
+                    </li>
+                    <li class="header__item header__item--hover"><a class="header__link" href="#">CROCHET</a> 
+                        <ul class="header__sublist"> 
+                            <li class="header__subitem"><a class="header__sublink" href="./crochet/amigurumis.html">AMIGURUMIS</a></li>
+                            <li class="header__subitem"><a class="header__sublink" href="./crochet/ropa.html">ROPA</a></li>
+                        </ul>
+                    </li>
+                    <li class="header__item"><a class="header__link" href="./contacto/contacto.html">CONTACTO</a></li>
+            </ul>
+            <form action="" method="get" enctype="text/plain">
+                <input class="header__input" type="search" name="s" id="idsearch" placeholder="  Buscar..." /> 
+            </form>    
+        </nav>   
+                              
+    )
+};
+
+export default Navigation;
